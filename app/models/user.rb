544da05c90b5ff95701ac47456e06e154be39cfd
yutaiko <yuta.ashikawa1984@gmail.com>
2020-08-03 16:53:22 +0900

    validation追加、name属性変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :email,    presence: true, uniqueness: true, format: { with: /\A\S+@\S+\.\S+\z/}
+  validates :password, presence: true, length: {minimum: 6}, format: { with: /\A[a-zA-Z0-9]+\z/}
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/}
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/}
+  validates :birthday, presence: true
+
 end
+
+
+
