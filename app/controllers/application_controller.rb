720383ebad9adb560e606017eed41daf7a2c26c9
yutaiko <yuta.ashikawa1984@gmail.com>
2020-08-04 15:29:25 +0900

    validation with_options、不要文削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,10 +24,3 @@ class ApplicationController < ActionController::Base
       ])
   end
 end
-
-def configure_permitted_parameters
-  devise_parameter_sanitizer.permit(:sign_up, keys: [
-    :追加カラム名,
-    :追加するカラム名
-  ])
-end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :email,    presence: true, uniqueness: true, format: { with: /\A\S+@\S+\.\S+\z/}
-  validates :password, presence: true, length: {minimum: 6}, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i}  
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/}
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/}
-  validates :birthday, presence: true
-
+  with_options presence: true do
+    validates :nickname
+    validates :email,           uniqueness: true, format: { with: /\A\S+@\S+\.\S+\z/}
+    validates :password,        length: {minimum: 6}, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i}  
+    validates :last_name,       format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
+    validates :first_name,      format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
+    validates :last_name_kana,  format: { with: /\A[ァ-ン]+\z/}
+    validates :first_name_kana, format: { with: /\A[ァ-ン]+\z/}
+    validates :birthday
+  end
 end
 
 
