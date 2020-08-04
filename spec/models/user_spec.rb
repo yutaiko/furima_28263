0cd6f251febea508dc187d95127b315630b642f3
yutaiko <yuta.ashikawa1984@gmail.com>
2020-08-04 11:29:53 +0900

    オブジェクト生成タイミング変更

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,13 @@
 require 'rails_helper'
 describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
   describe 'ユーザー新規登録' do    
     it "nicknameが空だと登録できない" do
-      user = FactoryBot.build(:user)
-      user.nickname = ""
-      user.valid?
-      expect(user.errors.full_messages).to include("Nickname can't be blank")
+      @user.nickname = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Nickname can't be blank")
     end
     it "emailが空では登録できない" do
     end
