1efb1bc0a66d8859bb3a2498792670be1f985990
yutaiko <yuta.ashikawa1984@gmail.com>
2020-08-04 11:22:14 +0900

    factorybot導入

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 describe User, type: :model do
   describe 'ユーザー新規登録' do    
     it "nicknameが空だと登録できない" do
-      user = User.new(nickname: "", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
+      user = FactoryBot.build(:user)
+      user.nickname = ""
       user.valid?
       expect(user.errors.full_messages).to include("Nickname can't be blank")
     end
