d6e2f8f1a293a0feb85c714c1042bc27043bd022
yutaiko <yuta.ashikawa1984@gmail.com>
2020-08-04 12:23:30 +0900

    user単体テストコード、新規登録成功_修正1

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,13 +6,12 @@ describe User, type: :model do
   describe 'ユーザー新規登録' do    
     context '新規登録成功' do
       it "nickname, email, first_name, last_name, first_name_kana, last_name_kana, birthdayが存在していれば登録ができる" do
-        @user.valid?
+        expect(@user).to be_valid
       end
       it "パスワードが6文字以上で登録ができる" do
         @user.password = "123456"
         @user.password_confirmation = "123456"
-        @user.valid?
-        binding.pry
+        expect(@user).to be_valid
       end
     end
 
