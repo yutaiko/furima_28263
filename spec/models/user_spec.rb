e1c8574da1b86fa318899e4e809673d5feb2c0c5
yutaiko <yuta.ashikawa1984@gmail.com>
2020-08-04 12:17:27 +0900

    userテーブル単体テストコード、example洗い出し

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,12 +4,45 @@ describe User, type: :model do
     @user = FactoryBot.build(:user)
   end
   describe 'ユーザー新規登録' do    
-    it "nicknameが空だと登録できない" do
-      @user.nickname = ""
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Nickname can't be blank")
+    context '新規登録成功' do
+      it "nickname, email, first_name, last_name, first_name_kana, last_name_kana, birthdayが存在していれば登録ができる" do
+      end
+      it "パスワードが6文字以上で登録ができる" do
+      end
     end
-    it "emailが空では登録できない" do
+
+    context '新規登録失敗' do
+      it "nicknameが空だと登録できない" do
+        @user.nickname = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
+      it "emailが空では登録できない" do
+      end
+      it "passwordが空では登録できない" do
+      end
+      it "first_nameが空では登録できない" do
+      end
+      it "last_nameが空では登録できない" do
+      end
+      it "first_name_kanaが空では登録できない" do
+      end
+      it "last_name_kanaが空では登録できない" do
+      end
+      it "birthdayが空では登録できない" do
+      end
+      it "パスワードが5文字以下では登録できない" do
+      end
+      it "パスワードが半角だけでは登録できない" do
+      end
+      it "パスワードが数字だけでは登録できない" do
+      end
+      it "確認用のパスワードを含めて２回入力しないと登録できない" do
+      end
+      it "ユーザー本名は全角（漢字・ひらがな・カタカナ）以外では登録できない" do
+      end
+      it "ユーザー本名フリガナは全角カタカナ以外では登録できない" do
+      end
     end
   end
 end
