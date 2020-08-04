d9de3156c653edcf057e577c0ba03f173dcdb415
yutaiko <yuta.ashikawa1984@gmail.com>
2020-08-04 12:01:25 +0900

    Faker,Gimei導入

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {"abe"}
-    email                 {"kkk@gmail.com"}
-    password              {"00000000"}
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
+    first_name            {Gimei::name.first.kanji}
+    last_name             {Gimei::name.last.kanji}
+    first_name_kana       {Gimei::name.first.katakana}
+    last_name_kana        {Gimei::name.last.katakana}
+    birthday              {Faker::Date.birthday}
   end
 end
