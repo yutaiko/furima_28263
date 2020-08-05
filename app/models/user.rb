f8014dbd2292884fbc2cdbba4e4d939e4514008f
yutaiko <yuta.ashikawa1984@gmail.com>
2020-08-05 11:16:07 +0900

    Association追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :items
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   Regex1 = /\A\S+@\S+\.\S+\z/.freeze # @が入る
