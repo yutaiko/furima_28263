26ab239328a3b379b146a3164511e1b869c0931e
yutaiko <yuta.ashikawa1984@gmail.com>
2020-08-05 11:43:01 +0900

    itemテーブル、ストロングパラメーター設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,27 @@
 class ItemsController < ApplicationController
   def index
   end
+
   def new
   end
+
+  def create
+    Item.create(item_params)
+  end
+
+  def item_params
+    params.require(:item).premit(
+      :image,
+      :name,
+      :description,
+      :category_id,
+      :condition_id,
+      :shipping_charge_id,
+      :shipping_origin_id,
+      :days_until_shipping_id,
+      :price,
+      :user
+    )
+  end
+
 end
