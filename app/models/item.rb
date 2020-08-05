765c7644caacbf16cc1521cdc4827805082737fb
yutaiko <yuta.ashikawa1984@gmail.com>
2020-08-05 17:31:13 +0900

    ActiveHash_categoryのアソシエーション、バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,9 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :genre
+  validates :genre_id, numericality: { other_than: 1 }   #ジャンルの選択が「--」の時は保存できないようにする
+
   belongs_to :user
   has_one_attached :image
+
 end
