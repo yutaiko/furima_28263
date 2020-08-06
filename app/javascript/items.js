5cc1976c2042fad8ff4c72a74989f5ea3a417703
yutaiko <yuta.ashikawa1984@gmail.com>
2020-08-06 12:34:06 +0900

    Jquery_導入テストファイル

---
+++ app/javascript/items.js
@@ -0,0 +1,7 @@
+// $(function () {
+//   $(document).on("mouseover", "h1", function () {
+//     $(this).css({ "color": "blue" });
+//   }).on("mouseout", "h2", function () {
+//     $(this).css({ "color": "" });
+//   })
+// });
