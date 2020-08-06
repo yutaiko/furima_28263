1c885cb602d2a39b0ade1210b2cb8b8cb5c867aa
yutaiko <yuta.ashikawa1984@gmail.com>
2020-08-06 17:18:22 +0900

    Rspec_環境設定

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    name                    { "Faker::One_piece.character" }
+    description             { "Faker::Text.text" }
+    category_id             { 1 }
+    condition_id            { 1 }
+    shipping_charge_id      { 1 }
+    shipping_origin_id      { 1 }
+    days_until_shipping_id  { 1 }
+    price                   { 10000 }
+    association :user
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
-
-RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Item, type: :model do
+  before do
+    @item = FactoryBot.build(:item)
+  end
+  describe '' do
+    context '' do
+      it '存在していれば登録ができる' do
+        binding.pry
+        # expect(@user).to be_valid
+      end
+      it 'パスワードが6文字以上で登録ができる' do
+        # @user.password = 'test5656'
+        # @user.password_confirmation = 'test5656'
+        # expect(@user).to be_valid
+      end
+    end
+  end
 end
