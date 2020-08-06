65b51f31a8d74630d9566e2d9e9663243a4bef09
yutaiko <yuta.ashikawa1984@gmail.com>
2020-08-06 15:44:59 +0900

    Jquery_販売手数料

--- app/javascript/items.js
+++
@@ -1,7 +0,0 @@
-// $(function () {
-//   $(document).on("mouseover", "h1", function () {
-//     $(this).css({ "color": "blue" });
-//   }).on("mouseout", "h2", function () {
-//     $(this).css({ "color": "" });
-//   })
-// });
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require('jquery')
-require('../items')
+require('../price')
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
