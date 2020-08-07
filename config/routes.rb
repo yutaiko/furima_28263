367ade6a63f3c67d7fd30ccc66935becfe448220
yutaiko <yuta.ashikawa1984@gmail.com>
2020-08-07 12:04:50 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root "items#index"
-  resources :items, only: [:index,:show,:new,:create,:edit]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :items, only: [:show,:new,:create,:edit]
 end
