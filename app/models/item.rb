ed026a65acf3c8088a1084480144c92092edafcd
yutaiko <yuta.ashikawa1984@gmail.com>
2020-08-07 11:52:39 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,19 +3,15 @@ class Item < ApplicationRecord
   belongs_to_active_hash :category
   validates :category_id, numericality: { other_than: 0 }
 
-  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :condition
   validates :condition_id,  numericality: { other_than: 0 }
 
-  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :shippingCharge
   validates :shipping_charge_id, numericality: { other_than: 0 }
 
-  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :shippingOrigin
   validates :shipping_origin_id, numericality: { other_than: 0 }
 
-  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :daysUntilShipping
   validates :days_until_shipping_id, numericality: { other_than: 0 }
 
