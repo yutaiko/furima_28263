daacd2d36dc2e7642da53b473a107b14b461d548
yutaiko <yuta.ashikawa1984@gmail.com>
2020-08-07 15:40:28 +0900

    一覧表示_全登録商品表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
 
   def index
-    @items = Item.find(8)
+    @items = Item.all.order("created_at DESC")
     # @items = Item.all.order("created_at DESC")
   end
 
