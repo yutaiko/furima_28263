f475aaa6fdf909ff0f6ad8d69796483d790d06b7
yutaiko <yuta.ashikawa1984@gmail.com>
2020-08-08 16:53:51 +0900

    エラー表示、商品説明表示、生成インスタンス調整

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit]
+  before_action :set_item, only: [:show, :edit, :update]
   before_action :set_item_id, only: [:show, :edit]
 
 
@@ -34,8 +34,7 @@ class ItemsController < ApplicationController
   end
 
   def update
-    item = Item.find(params[:id])
-    if item.update(item_params)
+    if @item.update(item_params)
       redirect_to item_path(@item)
     else
       render :edit
