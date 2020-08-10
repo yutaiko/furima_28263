82fe735f1b50236179b73c95cba2ba643c7f06c8
yutaiko <yuta.ashikawa1984@gmail.com>
2020-08-10 18:57:14 +0900

    ルーティング設定、ビュー追加、リンク設定

--- app/controllers/item_purchases_controller.rb
+++ app/controllers/item_purchases_controller.rb
@@ -1,2 +1,7 @@
 class ItemPurchasesController < ApplicationController
+  def index
+  end
+
+  def create
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "items#index"
   resources :items, except: [:index] do
-    resources :item_purchases, only: :create
+    resources :item_purchases, only: [:index, :create]
     resources :shipping_method, only: :create
   end
 end
