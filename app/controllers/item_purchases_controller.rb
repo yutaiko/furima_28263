8fad17e9865f221aabc94a1fb5e7a5c89a9ba973
yutaiko <yuta.ashikawa1984@gmail.com>
2020-08-10 19:11:23 +0900

    購入ページ、商品表示設定

--- app/controllers/item_purchases_controller.rb
+++ app/controllers/item_purchases_controller.rb
@@ -1,7 +1,26 @@
 class ItemPurchasesController < ApplicationController
+  before_action :set_item, only: [:index, :create]
+  before_action :set_item_id, only: [:index]
+
   def index
   end
 
   def create
   end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def set_item_id
+    @user = User.find(@item.user_id)
+    @category = Category.find(@item.category_id)
+    @condition = Condition.find(@item.condition_id)
+    @shippingCharge = ShippingCharge.find(@item.shipping_charge_id)
+    @shippingOrigin = ShippingOrigin.find(@item.shipping_origin_id)
+    @daysUntilShipping = DaysUntilShipping.find(@item.days_until_shipping_id)
+  end
+  
 end
