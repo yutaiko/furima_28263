5ee25e9d749f41dec5305b0034380332ea9e8240
yutaiko <yuta.ashikawa1984@gmail.com>
2020-08-10 12:14:12 +0900

    destroyアクション修正
    インスタンス生成をbefore_action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :set_item_id, only: [:show, :edit]
   before_action :correct_user, only: [:edit, :update]
 
@@ -41,8 +41,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    if item.destroy
+    if @item.destroy
       redirect_to root_path
     else
       render :show
