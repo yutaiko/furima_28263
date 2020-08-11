243a450433aef1091d508a3e83e08cfcf761c9bb
yutaiko <yuta.ashikawa1984@gmail.com>
2020-08-11 11:03:18 +0900

    実装条件２
    出品者はURLを直接入力して購入ページに遷移しようとすると、トップページに遷移すること

--- app/controllers/item_purchases_controller.rb
+++ app/controllers/item_purchases_controller.rb
@@ -2,6 +2,7 @@ class ItemPurchasesController < ApplicationController
   before_action :set_item, only: [:index, :create]
   before_action :set_item_id, only: [:index]
   before_action :authenticate_user!, only: [:index, :create]
+  before_action :same_user_do_not_purchase, only: [:index, :create]
 
   def index
   end
@@ -23,5 +24,10 @@ class ItemPurchasesController < ApplicationController
     @shippingOrigin = ShippingOrigin.find(@item.shipping_origin_id)
     @daysUntilShipping = DaysUntilShipping.find(@item.days_until_shipping_id)
   end
-  
+
+  def same_user_do_not_purchase
+    if current_user.id == @item.user_id
+      redirect_to root_path
+    end
+  end
 end
